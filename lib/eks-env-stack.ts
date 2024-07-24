c4f3e85509620b9a0d6f5df6c9b9179eb9bcd9d1
dpstrip <dpstrip@gmail.com>
2024-07-24 16:06:25 -0500

    Update eks-env-stack.ts

--- lib/eks-env-stack.ts
+++ lib/eks-env-stack.ts
@@ -8,81 +8,92 @@ import * as assets from 'aws-cdk-lib/aws-s3-assets';
 export class EksEnvStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    let  sVpcid: string;
-    sVpcid = "vpc-08b4163ec288bd766cdk synth";
+   // Lookup existing VPC
+    const vpc = ec2.Vpc.fromLookup(this, 'VPC', {
+      vpcId: props.vpcId,
+    });
 
-     //get VPC object 
-    const vpc = ec2.Vpc.fromLookup(this, 'VPC', {vpcId: sVpcid});
-
-    //Create iam role
-    const clusterAdmin = new iam.Role(this, 'AdminRole', {
+    // Create IAM role for EKS cluster
+    const clusterAdminRole = new iam.Role(this, 'AdminRole', {
       assumedBy: new iam.AccountRootPrincipal(),
     });
 
-    const cluster = new eks.Cluster(this, 'eksCluster',{
+    // Create the EKS cluster
+    const cluster = new eks.Cluster(this, 'Cluster', {
       vpc,
-      mastersRole: clusterAdmin,
-      defaultCapacity: 2,
-      defaultCapacityInstance: new ec2.InstanceType('t3.small'),
+      defaultCapacity: 1, // number of nodes
+      defaultCapacityInstance: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO), // small server
+      mastersRole: clusterAdminRole,
       version: eks.KubernetesVersion.V1_30,
-      endpointAccess: eks.EndpointAccess.PRIVATE,
-      vpcSubnets: [{ 
-          subnetType: ec2.SubnetType.PRIVATE_ISOLATED 
-      }], 
     });
 
-    //add ons
-    const kubeProxy = new eks.CfnAddon(this, 'addonKubeProxy',{
-            addonName: "kube-proxy",
-            clusterName: cluster.clusterName,
-          });
-    const coreDNS = new eks.CfnAddon(this, 'addoncoreDNS',{
-            addonName: "coredns",
-            clusterName: cluster.clusterName,
-          });
-    const vpcCni = new eks.CfnAddon(this, 'addonVpcCni',{
-            addonName: "vpc-cni",
-            clusterName: cluster.clusterName,
-          });
+    // Add EKS addons
+    cluster.addCniAddOn();
+    cluster.addCoreDnsAddOn();
+    cluster.addKubeProxyAddOn();
 
-          ////create new policy
-          const policy = new iam.PolicyStatement({
-            actions:['esk:*'],
-            resources: ['*']
-          });
+    // Create OIDC provider for the EKS cluster
+    const oidcProvider = new eks.OpenIdConnectProvider(this, 'OIDCProvider', {
+      cluster,
+    });
 
-          clusterAdmin.addToPolicy(policy);
+    // Bastion host setup
+    const bastion = new ec2.BastionHostLinux(this, 'BastionHost', {
+      vpc,
+      instanceType: new ec2.InstanceType('t3.micro'),
+    });
 
-          //create Bastion host
-          const asset = new assets.Asset(this, 'S3Asset', {
-            path: 'assets/kubectl'
-          });
-        
-          const userData = ec2.UserData.forLinux();
-          userData.addS3DownloadCommand({
-            bucket: asset.bucket,
-            bucketKey: asset.s3ObjectKey,
-            localFile: '/tmp/kubectl'
-          });
-          userData.addCommands(
-            'chmod +x /tmp/kubectl',
-            'cp /tmp/kubectl /usr/local/bin'
-          );
-        
-          
-  const host = new ec2.BastionHostLinux(this, 'Bastion', { 
-            vpc,
-            requireImdsv2: true,
-            machineImage: ec2.MachineImage.latestAmazonLinux2023 ({
-            userData,
-            //generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
-            })
-          });
-        
-   host.role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'));
-   host.role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
-        
+    // Asset to deploy kubectl binary
+    const kubectlAsset = new s3assets.Asset(this, 'KubectlAsset', {
+      path: './assets/kubectl', // assuming kubectl binary is in the assets directory
+    });
 
+    // Setup IAM role for Bastion host to access EKS cluster
+    const bastionRole = new iam.Role(this, 'BastionRole', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+    });
+
+    bastionRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonEKSClusterPolicy')
+    );
+    bastionRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonEKSWorkerNodePolicy')
+    );
+    bastionRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonEC2ContainerRegistryReadOnly')
+    );
+
+    // Attach the role to the bastion host
+    bastion.instance.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ['eks:*'],
+        resources: ['*'],
+      })
+    );
+
+    // Add user data to Bastion host to download kubectl
+    bastion.instance.userData.addS3DownloadCommand({
+      bucket: kubectlAsset.bucket,
+      bucketKey: kubectlAsset.s3ObjectKey,
+      localFile: '/usr/local/bin/kubectl',
+    });
+
+    bastion.instance.userData.addExecuteFileCommand({
+      filePath: '/usr/local/bin/kubectl',
+      arguments: '--version', // to verify installation
+    });
+
+    // Allow Bastion host to connect to EKS cluster
+    cluster.awsAuth.addMastersRole(bastionRole);
+
+    // Outputs
+    new cdk.CfnOutput(this, 'ClusterName', {
+      value: cluster.clusterName,
+    });
+
+    new cdk.CfnOutput(this, 'BastionHostPublicDNS', {
+      value: bastion.instancePublicDnsName,
+    });
 
   }
 }
